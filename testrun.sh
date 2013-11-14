5763db3aade11683a8c4003bf63e3b220229e56c
Daniel Tai <cooldll694@gmail.com>
2013-11-14 21:44:59 +0800

    beautify test script

--- testrun.sh
+++ testrun.sh
@@ -5,9 +5,15 @@ runs=50
 for size in 1 2 5 10 100 1000 100000 1000000 10000000 105000000
 do
     stime=0
+    echo -n "Testing size=${size}: " 1>&2
     for i in $(seq 1 $runs);
     do
-        echo "Run $i/$runs of size $size..." 1>&2
+        if [ $(($i % 10)) -eq 0 ]
+        then
+            echo -n "$i" 1>&2
+        else
+            echo -n '.'
+        fi
         ctime=$(./reduction $RANDOM $size | awk '/^Kernel/ { print $3 }')
         if [ "$ctime" == "" ] 
         then
@@ -16,6 +22,7 @@ do
         fi
         stime=$(( $stime + $ctime ))
     done
+    echo ""
     mtime=$(echo "$stime / $runs" | bc)
 #    echo "$size,$mtime"
     echo "size=$size mean_time=$mtime"
